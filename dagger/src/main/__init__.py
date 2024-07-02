83cdc3b3da7fb99810a5d652cf6415395e61b2ec
Reto Galante <galante@puzzle.ch>
2024-07-02 16:56:23 +0200

    Set python_version

--- dagger/src/main/__init__.py
+++ dagger/src/main/__init__.py
@@ -3,6 +3,7 @@
 This module has been generated via dagger init and serves as a reference to
 basic module structure as you get started with Dagger.
 """
+from typing import Self
 
 import dagger
 from dagger import dag, function, object_type
@@ -11,28 +12,35 @@ from dagger import dag, function, object_type
 @object_type
 class BalanceR:
 
+    python_version: str = "3"
+
     @function
-    def chuck_norris(self, source: dagger.Directory, python: str = ""):
+    def with_python(self, version: str | None) -> Self:
+        if version:
+            self.python_version = version
+        return self
+
+    @function
+    def chuck_norris(self, source: dagger.Directory):
         """Run lint and test (WITHOUT FAILING, EVER!)"""
-        self.test(source, python)
-        self.lint(source, python)
-
+        self.test(source)
+        self.lint(source)
 
     @function
-    async def test(self, source: dagger.Directory, python: str = "") -> str:
+    async def test(self, source: dagger.Directory) -> str:
         """Return the result of running unit tests"""
         return await (
-            self.build_env(source, python)
+            self.build_env(source)
             .with_exec(["pip", "install", "pytest"])
             .with_exec(["pytest", "balancer_test.py"])
             .stdout()
         )
 
     @function
-    async def lint(self, source: dagger.Directory, python: str = "") -> str:
+    async def lint(self, source: dagger.Directory) -> str:
         """Return the result of the linting"""
         return await (
-            self.build_env(source, python)
+            self.build_env(source)
             .with_exec(["pip", "install", "flake8"])
             .with_exec(["flake8", "balancer.py", "--count", "--select=E9,F63,F7,F82", "--show-source", "--statistics"])
             .with_exec(["flake8", "balancer.py", "--count", "--exit-zero", "--max-complexity=10", "--max-line-length=127", "--statistics"])
@@ -40,12 +48,11 @@ class BalanceR:
         )
 
     @function
-    def build_env(self, source: dagger.Directory, python: str = "") -> dagger.Container:
+    def build_env(self, source: dagger.Directory) -> dagger.Container:
         """Build a ready-to-use development environment"""
-        python_version = python if python != "" else "3"
         return (
             dag.container()
-            .from_("python:"+python_version+"-alpine")
+            .from_("python:"+self.python_version+"-alpine")
             .with_directory("/src", source)
             .with_workdir("/src")
             .with_exec(["pip", "install", "--upgrade", "pip"])
